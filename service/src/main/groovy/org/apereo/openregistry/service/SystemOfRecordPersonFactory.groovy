16f9661a4b0c0324c3447974f3d6aed3f90b8fe9
Dmitriy Kopylenko <dima767@gmail.com>
2014-03-05 11:00:04 -0500

    Initial push of a service module. WIP...

---
+++ service/src/main/groovy/org/apereo/openregistry/service/SystemOfRecordPersonFactory.groovy
@@ -0,0 +1,11 @@
+package org.apereo.openregistry.service
+
+import org.apereo.openregistry.model.SystemOfRecordPerson
+
+/**
+ *
+ * Factory for creating standardized {@link org.apereo.openregistry.model.SystemOfRecordPerson}s
+ */
+public interface SystemOfRecordPersonFactory {
+
+}
