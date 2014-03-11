b4894d3163d92ac31b702ac0276498303d596b68
Dmitriy Kopylenko <dima767@gmail.com>
2014-03-11 12:37:33 -0400

    Spring Boot Java config and wiring. WIP...

--- web/src/main/groovy/org/apereo/openregistry/cifer/rest/OpenRegistrySystemOfRecordDataResource.groovy
+++ web/src/main/groovy/org/apereo/openregistry/cifer/rest/OpenRegistrySystemOfRecordDataResource.groovy
@@ -1,5 +1,8 @@
 package org.apereo.openregistry.cifer.rest
 
+import groovy.util.logging.Slf4j
+import org.apereo.openregistry.service.SystemOfRecordPersonFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,15 +15,23 @@ import org.springframework.web.bind.annotation.RestController
  */
 @RestController
 @RequestMapping("/v1")
+@Slf4j
 class OpenRegistrySystemOfRecordDataResource {
 
     //Injected from openregistry.properties or if not present uses the default value defined inline (after the (:) colon token)
+    //This is here just to test Boot's capabilities in the initial stages of development. Will be removed later...
     @Value("\${openregistry.version:2.0-M1-SNAPSHOT}")
     private orVersion
+
+    @Autowired
+    private SystemOfRecordPersonFactory systemOfRecordPersonFactory
 
     @RequestMapping(method = RequestMethod.PUT, value = "/sorPeople/{sor}/{sorId}")
     @ResponseBody
     def createOrUpdatePerson(@PathVariable("sor") String sor, @PathVariable("sorId") String personSorId) {
+        //TODO: figure out Boot's logback config for app level logging levels
+        log.debug("Calling PUT /sorPeople/* ...")
+        log.info("Spring Boot has injected [${this.systemOfRecordPersonFactory}")
         //The presence of @ResponseBody and Jackson Message Converter that Boot adds to the classpath
         //will cause this Map to serialized as a JSON blob (application/json Content-Type) on the client side!
         [openRegistryVersion: this.orVersion,
--- web/src/main/groovy/org/apereo/openregistry/springboot/Application.groovy
+++ web/src/main/groovy/org/apereo/openregistry/springboot/Application.groovy
@@ -1,9 +1,12 @@
 package org.apereo.openregistry.springboot
 
+import org.apereo.openregistry.model.SystemOfRecordPerson
+import org.apereo.openregistry.service.SystemOfRecordPersonFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.web.SpringBootServletInitializer
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
@@ -26,5 +29,28 @@ class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         application.sources(applicationClass)
+    }
+
+    /**
+     * This is Spring's Java config method. The bean with name 'systemOfRecordPersonFactory'
+     * will be created in the parent app ctx and will be a subject for autowiring - will be injected into those beans
+     * that express dependency on it via @Autowired or @Inject
+     */
+    @Bean
+    SystemOfRecordPersonFactory systemOfRecordPersonFactory() {
+        //Just for initial Boot's testing at the beginning of
+        //development. Will be replaced with real implementation later...
+        new SystemOfRecordPersonFactory() {
+
+            @Override
+            SystemOfRecordPerson createFrom(Map<Object, Object> systemOfRecordPersonData) {
+                new SystemOfRecordPerson(systemOfRecordPersonData)
+            }
+
+            @Override
+            String toString() {
+                "Mock SystemOfRecordPersonFactory"
+            }
+        }
     }
 }
