f39902b0e45d6984fb704aa5013826b67e55d10b
Jj <jj@scaldingspoon.com>
2014-03-19 10:23:40 -0500

    [ORII-21 Standardization Processor and Service] WIP

---
+++ gorm-service/src/main/groovy/org/apereo/openregistry/processor/gorm/StandardizationProcessor.groovy
@@ -0,0 +1,12 @@
+package org.apereo.openregistry.processor.gorm
+
+import org.apereo.openregistry.service.OpenRegistryProcessor
+import org.apereo.openregistry.service.OpenRegistryProcessorContext
+
+class StandardizationProcessor implements OpenRegistryProcessor {
+    @Override
+    OpenRegistryProcessorContext process(OpenRegistryProcessorContext processorContext) {
+        // TODO: implement
+        return processorContext
+    }
+}
