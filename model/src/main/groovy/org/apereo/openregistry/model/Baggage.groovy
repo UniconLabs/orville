4ea3b297b4adae39acb84523e9df07666cb506c8
Jj <jj@scaldingspoon.com>
2014-04-07 13:17:26 -0500

    [NOJIRA] removed other models.. now we are simple updated baggage model

--- model/src/main/groovy/org/apereo/openregistry/model/Baggage.groovy
+++ model/src/main/groovy/org/apereo/openregistry/model/Baggage.groovy
@@ -1,6 +1,8 @@
 package org.apereo.openregistry.model
 
 import grails.persistence.Entity
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
 
 /**
  * Container for extra information about a person from a system of record
@@ -9,4 +11,30 @@ import grails.persistence.Entity
 class Baggage {
     SystemOfRecord systemOfRecord
     Map<String,Object> baggage
+    String baggageAsString
+
+    static constraints = {
+        baggageAsString maxSize: 64000
+    }
+
+    static transients = ['baggage']
+
+    def afterLoad() {
+        baggage = new JsonSlurper().parseText(baggageAsString)
+    }
+
+    def beforeInsert() {
+        baggageAsString = baggageToString()
+    }
+
+    def beforeUpdate() {
+        baggageAsString = baggageToString()
+    }
+
+    String baggageToString() {
+        new JsonBuilder().with {
+            it baggage
+            return it
+        }.toString()
+    }
 }
