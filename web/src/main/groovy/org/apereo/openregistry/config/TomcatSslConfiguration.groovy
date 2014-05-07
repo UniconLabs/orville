9e610c70274fb40fbf422f926cd4b579aee8b209
Dmitriy Kopylenko <dima767@gmail.com>
2014-05-07 18:30:13 -0400

    WIP2

--- web/src/main/groovy/org/apereo/openregistry/config/TomcatSslConfiguration.groovy
+++ web/src/main/groovy/org/apereo/openregistry/config/TomcatSslConfiguration.groovy
@@ -1,6 +1,8 @@
 package org.apereo.openregistry.config
 
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer
 import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory
@@ -14,24 +16,32 @@ import org.springframework.context.annotation.Configuration
  * @Unicon inc
  */
 @Configuration
+@Slf4j
 class TomcatSslConfiguration {
 
     @Bean
-    public EmbeddedServletContainerCustomizer containerCustomizer(@Value('${ssl.keystore.file}') String keystoreFile,
-                                                                  @Value('${ssl.keystore.password}') String keystorePassword,
-                                                                  @Value('${ssl.keystore.type}') String keystoreType,
-                                                                  @Value('${ssl.keystore.alias}') String keystoreAlias) throws FileNotFoundException {
+    @ConditionalOnExpression('${ssl.enabled:false}')
+    public EmbeddedServletContainerCustomizer tomcatSslConnectorAddingServletContainerCustomizer(
+            @Value('${ssl.keystore.file}') String keystoreFile,
+            @Value('${ssl.keystore.password}') String keystorePassword,
+            @Value('${ssl.keystore.type}') String keystoreType,
+            @Value('${ssl.keystore.alias}') String keystoreAlias) throws FileNotFoundException {
+
+        log.debug("SSL config flag is set. Creating 'tomcatSslConnectorAddingServletContainerCustomizer' bean...")
+
         return {
             (it as TomcatEmbeddedServletContainerFactory).addConnectorCustomizers({
-                it.setSecure(true);
-                it.setScheme('https');
-                it.setAttribute('keystoreFile', keystoreFile);
-                it.setAttribute('keystorePass', keystorePassword);
-                it.setAttribute('keystoreType', keystoreType);
-                it.setAttribute('keyAlias', keystoreAlias);
-                it.setAttribute('clientAuth', false);
-                it.setAttribute('sslProtocol', 'TLS');
-                it.setAttribute('SSLEnabled', true);
+                it.setSecure(true)
+                it.setScheme('https')
+                it.setAttribute('keystoreFile', keystoreFile)
+                it.setAttribute('keystorePass', keystorePassword)
+                it.setAttribute('keystoreType', keystoreType)
+                it.setAttribute('keyAlias', keystoreAlias)
+                it.setAttribute('clientAuth', false)
+                it.setAttribute('sslProtocol', 'TLS')
+                it.setAttribute('SSLEnabled', true)
+                //TODO: perhaps externalize the port config value?
+                it.setAttribute('port', 8443)
             } as TomcatConnectorCustomizer)
         } as EmbeddedServletContainerCustomizer
     }
